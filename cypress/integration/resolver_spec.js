ba4ae3f36f087b24ed1b45755dd46da0bc7beb12
zaidarivai <zaida.rivai@danubetech.com>
2022-03-07 13:05:28 +0100

    perform a not found test

--- cypress/integration/resolver_spec.js
+++ cypress/integration/resolver_spec.js
@@ -1,71 +1,65 @@
-// it.skip("Test 1: Normal DID Resolution Result", () => {
-//     cy.request({
-//         method: "GET",
-//         url: "https://dev.uniresolver.io/1.0/identifiers/did:sov:WRfXPg8dantKVubE3HX8pw",
-//         failOnStatusCode: false,
-//     }).as("identifiers");
-//
-//     cy.get('@identifiers').should((response) => {
-//         console.log(response.body)
-//         expect(response.status).to.eq(200)
-//         expect(response.body).to.have.property('didDocument')
-//         expect(response.body).to.have.property('didResolutionMetadata')
-//         expect(response.body).to.have.property('didDocumentMetadata')
-//     });
-// });
-
 describe("Test 1: Normal DID Resolution Result", () => {
-    beforeEach(() => {
-        cy.request({
-            method:"GET",
-            url: "https://dev.uniresolver.io/1.0/identifiers/did:sov:WRfXPg8dantKVubE3HX8pw",
-        }).as("request")
+  beforeEach(() => {
+    cy.request({
+      method: "GET",
+      url: "https://dev.uniresolver.io/1.0/identifiers/did:sov:WRfXPg8dantKVubE3HX8pw",
+    }).as("request");
+  });
+
+  it("Should have response status 200", () => {
+    cy.get("@request").then((response) => {
+      expect(response.status).to.eq(200);
     });
+  });
 
-    it("Should have response status 200", () => {
-        cy.get("@request").then((response)=> {
-            expect(response.status).to.eq(200)
-    })
+  it("Should have property didDocument", () => {
+    cy.get("@request").then((response) => {
+      expect(response.body).to.have.property("didDocument");
+    });
+  });
+  it("Should have property didResolutionMetadata", () => {
+    cy.get("@request").then((response) => {
+      expect(response.body).to.have.property("didResolutionMetadata");
+    });
+  });
+  it("Should have property didDocumentMetadata", () => {
+    cy.get("@request").then((response) => {
+      expect(response.body).to.have.property("didDocumentMetadata");
+    });
+  });
 });
-});
-
 
 describe("Test 2: Only DID document", () => {
-    beforeEach(() => {
-        cy.request({
-            method:"GET",
-            url: "https://dev.uniresolver.io/1.0/identifiers/did:sov:WRfXPg8dantKVubE3HX8pw",
-            headers: {"Accept": "application/did+ld+json"}
-        }).as("headers")
+  beforeEach(() => {
+    cy.request({
+      method: "GET",
+      url: "https://dev.uniresolver.io/1.0/identifiers/did:sov:WRfXPg8dantKVubE3HX8pw",
+      headers: { Accept: "application/did+ld+json" },
+    }).as("headers");
+  });
+
+  it("Should have response status 200", () => {
+    cy.get("@headers").then((response) => {
+      console.log(response.body);
+      expect(response.status).to.eq(200);
     });
+  });
 
-        it("Should have response status 200", () => {
-            cy.get("@headers").then((response)=> {
-                console.log(response.body)
-                expect(response.status).to.eq(200)
-            })
-        });
-
-        it("Should not have property didDocument", () => {
-            cy.get("@headers").then((response)=> {
-                expect(response.body).not.to.have.property('didDocument')
-            })
-        });
+  it("Should not have property didDocument", () => {
+    cy.get("@headers").then((response) => {
+      expect(response.body).not.to.have.property("didDocument");
+    });
+  });
 });
 
-
-
-//
-// it.skip("Test 3: Not found", () => {
-//     cy.request({
-//         method: "GET",
-//         url: "https://dev.uniresolver.io/1.0/identifiers/did:sov:WRfXPg8dantKVubE3HX8pw",
-//         failOnStatusCode: false,
-//     }).as("identifiers");
-//
-//     cy.get('@identifiers').should((response) => {
-//         console.log(JSON.stringify(response))
-//         expect(response.status).to.eq(200)
-//         expect(response.body.didResolutionMetadata.error).to.equal('notFound')
-//     });
-// });
+describe.only("Test 3: Not found", () => {
+  it("Tests an error", () => {
+    cy.request({
+      method: "GET",
+      url: "https://dev.uniresolver.io/1.0/identifiers/did:sov:WRfXPg8dantKVubE3HX8pwtest",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+    });
+  });
+});
